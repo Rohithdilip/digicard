2b1ec4c7490a3a0873567fc81e0fdf1ac33d7f78
VIGNESH K S <iamvvv0@gmail.com>
2021-06-26 22:35:53 +0530

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/*.svelte", "./src/*.html", "./src/*.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
